8f53b9dedf27b28199f6e5e61799abaac21cf6da
Rahul T <rahulmanoj@cet.ac.in>
2021-05-15 15:07:45 +0530

    contact page added

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,23 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const mainNavLinks = document.querySelectorAll('.abc');
+const sections = document.querySelectorAll('section')
+const navLink = document.querySelectorAll(".nav-link");
+
+
+window.addEventListener('scroll', e => {
+    let fromTop = window.scrollY + 10;
+
+    mainNavLinks.forEach(link => {
+        console.log(link.hash);
+        let section = document.querySelector(link.hash)
+        if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop){
+            link.classList.add('current')
+        } else{
+            link.classList.remove('current')
+        }
+    })
+})
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -8,7 +26,6 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
